1e645dbcd656c123df67e5a137d605b4cd71742e
Bruno Carlin <brunotestaguzza@gmail.com>
2018-07-08 03:42:30 -0300

    Ok I guess graphs in python are gone, sad

--- Magic.R
+++ Magic.R
@@ -1,6 +1,27 @@
 library(reticulate)
-
+library(githubinstall)
+library(devtools)
 Sys.setenv(LANG = "en")
 
 
 devtools::install("basictemplate")
+
+
+install_github(c("brunocarlin/Templater", "brunocarlin/basictemplate"))
+
+R.Version()
+
+update.packages()
+
+
+if(!require(installr)) {
+  install.packages("installr"); require(installr)} #load / install+load installr
+
+# using the package:
+updateR()
+
+githubinstall("Templater")
+
+gh_list_packages("brunocarlin")
+
+gh_install_packages("basictemplate")
